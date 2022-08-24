ff245c9fce81b23049ee20a74be76f4e8b4113d7
Pavlo Kysliak <pavel.kis0505@gmail.com>
2022-08-24 23:15:03 +0300

    done

--- src/components/ExpensesTable/ExpensesTable.jsx
+++ src/components/ExpensesTable/ExpensesTable.jsx
@@ -1,21 +1,37 @@
 import s from './ExpensesTable.module.css';
 import sprit from '../../assets/icons/sprite.svg';
+// import { useSelector } from 'react-redux';
+
 function ExpensesTable(props) {
+  //   const date = useSelector();
   return (
     <div className={s.table}>
       <h3 className={s.categori}>
-        <span>Date</span> <span>Description</span> <span>category</span>
-        <span>Sum</span> <span></span>
+        <span className={s.dates}>Date</span>
+        <span className={s.description}>Description</span>
+        <span className={s.category}>category</span>
+        <span className={s.sums}>Sum</span> <span></span>
       </h3>
       <ul className={s['inform-list']}>
         <li className={s.inform}>
-          <div>
-            <span>05.09.2019</span>
-            <span>Моя зп</span>
-          </div>
+          <span className={s.date}>05.09.2019</span>
+          <span className={s.desc}>Моя зп</span>
 
-          <span>Зп</span>
-          <span>20 000.00 грн.</span>
+          <span className={s.categ}>Зп</span>
+          <span className={s.sum}>20 000.00 грн.</span>
+          <button className={s.btn} type="button">
+            <svg width="18" height="18">
+              <use href={sprit + '#icon-delete'}></use>
+            </svg>
+          </button>
+        </li>
+
+        <li className={s.inform}>
+          <span className={s.date}>05.09.2019</span>
+          <span className={s.desc}>% на остаток на карте</span>
+
+          <span className={s.categ}>Доп.доходы</span>
+          <span className={s.sum}>500.00 грн.</span>
           <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
@@ -23,10 +39,11 @@ function ExpensesTable(props) {
           </button>
         </li>
         <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
+          <span className={s.date}></span>
+          <span className={s.desc}></span>
+
+          <span className={s.categ}></span>
+          <span className={s.sum}></span>
           <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
@@ -34,10 +51,11 @@ function ExpensesTable(props) {
           </button>
         </li>
         <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
+          <span className={s.date}></span>
+          <span className={s.desc}></span>
+
+          <span className={s.categ}></span>
+          <span className={s.sum}></span>
           <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
@@ -45,10 +63,11 @@ function ExpensesTable(props) {
           </button>
         </li>
         <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
+          <span className={s.date}></span>
+          <span className={s.desc}></span>
+
+          <span className={s.categ}></span>
+          <span className={s.sum}></span>
           <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
@@ -56,112 +75,64 @@ function ExpensesTable(props) {
           </button>
         </li>
         <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">
+          <span className={s.date}></span>
+          <span className={s.desc}></span>
+
+          <span className={s.categ}></span>
+          <span className={s.sum}></span>
+          <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
             </svg>
           </button>
         </li>
         <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">
+          <span className={s.date}></span>
+          <span className={s.desc}></span>
+
+          <span className={s.categ}></span>
+          <span className={s.sum}></span>
+          <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
             </svg>
           </button>
         </li>
         <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">
+          <span className={s.date}></span>
+          <span className={s.desc}></span>
+
+          <span className={s.categ}></span>
+          <span className={s.sum}></span>
+          <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
             </svg>
           </button>
         </li>
         <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">
+          <span className={s.date}></span>
+          <span className={s.desc}></span>
+
+          <span className={s.categ}></span>
+          <span className={s.sum}></span>
+          <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
             </svg>
           </button>
         </li>
         <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">
+          <span className={s.date}></span>
+          <span className={s.desc}></span>
+
+          <span className={s.categ}></span>
+          <span className={s.sum}></span>
+          <button className={s.btn} type="button">
             <svg width="18" height="18">
               <use href={sprit + '#icon-delete'}></use>
             </svg>
           </button>
-        </li>
-        <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">close</button>
-        </li>
-        <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">close</button>
-        </li>
-        <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">close</button>
-        </li>
-        <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">
-            <svg width="18" height="18">
-              <use href={sprit + '#icon-delete'}></use>
-            </svg>
-          </button>
-        </li>
-        <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">close</button>
-        </li>
-        <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">close</button>
-        </li>
-        <li className={s.inform}>
-          <span>05.09.2019</span>
-          <span> % на остаток на карте</span>
-          <span>Доп.доходы</span>
-          <span>500.00 грн.</span>
-          <button type="button">close</button>
         </li>
       </ul>
     </div>
